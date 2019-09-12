e1a1670a4f9a028fcf6f11d4f0255fdd101205fa
salvob <tholalak@yahoo.it>
2019-09-12 17:05:42 +0200

    refactoring probe node

--- CollisionProbe.gd
+++ CollisionProbe.gd
@@ -1,9 +1,15 @@
 extends Node
 
-var host
+onready var host = get_parent()
+export var manager_name = "CollisionManager"
+var collision_manager
 
 func _ready():
-	host = get_parent()
+	# Generic way to get the manager. Maybe
+	for manager in get_tree().get_nodes_in_group("managers"):
+		if manager_name == manager.name:
+			collision_manager = manager
+			break
 	
 	if host is RigidBody2D:
 		# automagic
@@ -11,8 +17,12 @@ func _ready():
 		if host.contacts_reported <= 0:
 			host.contacts_reported = 1
 			
-		host.connect("body_entered", self, "_on_body_entered")
-		
-func _on_body_entered(body):
-	get_tree().emit_signal("collision")
-	
+		host.connect('body_entered', collision_manager, '_on_collision_enter', [host])
+		# connect('body_exited', collision_manager, '_on_collision_exit', [host])
+		host.connect('body_shape_entered', collision_manager, '_on_collision_enter', [host])
+		# connect('body_shape_exited', collision_manager, '_on_collision_exit', [host])
+	elif host is Area2D:
+		host.connect("area_entered", collision_manager, "_on_collision_enter", [host])
+		host.connect("area_exited", collision_manager, "_on_collision_exited", [host])
+		host.connect("body_entered", collision_manager, "_on_collision_enter", [host])
+		host.connect("body_exited", collision_manager, "_on_collision_exited", [host])
--- RigidBody2DEntity.gd
+++ RigidBody2DEntity.gd
@@ -1,10 +1,18 @@
 extends RigidBody2D
 
 signal collision_enter
+export var cm_node_path : NodePath 
+onready var collision_manager = set_collision_manager(cm_node_path)
 
-func _ready():
-	connect('body_entered', self, '_on_body_entered')
+func set_collision_manager(node_path: NodePath):
+	return get_node(node_path)
 	
-func _on_body_entered(body):
-	emit_signal("collision_enter", body, self)
+func _ready():
+	connect('body_entered', collision_manager, '_on_collision_enter', [self])
+	# connect('body_exited', collision_manager, '_on_collision_exit', [self])
+	connect('body_shape_entered', collision_manager, '_on_collision_enter', [self])
+	# connect('body_shape_exited', collision_manager, '_on_collision_exit', [self])
+	
+	
+	
 	
