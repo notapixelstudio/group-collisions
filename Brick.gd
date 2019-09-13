788a03fe846a007bc21d1de0726cf3efb4f48dcc
nitaku <matteo.abrate@gmail.com>
2019-09-13 15:53:36 +0200

    graphics

---
+++ Brick.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+
+var default_sprite = preload('res://assets/unbreakable.png')
+var breakable_sprite = preload('res://assets/breakable.png')
+
+func _on_tag_added(tag):
+	if TAG.has_tag(self, 'breakable'):
+		$Sprite.texture = breakable_sprite
+		
+func _on_tag_removed(tag):
+	if not (TAG.has_tag(self, 'breakable')):
+		$Sprite.texture = default_sprite
+		
